7eb136749cbcb7fade177b29b76ba25987d23cf0
Draken-Dragon <bravotspiderman@gmail.com>
2024-07-11 12:44:48 +0530

    Update Home section.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,56 @@
+import { Fragment, useEffect, useMemo, useState } from "react";
+import { ReactTyped } from "react-typed";
+
 import "../styles/HomePage.css";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  const [showCursor, setShowCursor] = useState(false);
+
+  useEffect(() => {
+    const cursorTimeout = setTimeout(() => {
+      setShowCursor(true);
+    }, 2000);
+
+    return () => {
+      clearTimeout(cursorTimeout);
+    };
+  });
+
+  const services = [
+    "Digital Marketing",
+    "Performance Marketing",
+    "Influential Marketing",
+    "SEO",
+    "Production",
+    "Email Marketing",
+    "Public Relations",
+  ];
+
   return (
-    <section className="home-page">
-      <div className="home-content container">
-        <h1 className="page-heading">AtBuzz</h1>
-      </div>
-    </section>
+    <Fragment>
+      <section className="home-page" id="home">
+        <div className="home-content container">
+          <h1 className="page-heading">
+            <span className="first-half">Are You</span>
+            <span className="second-half">Looking For</span>
+          </h1>
+          {showCursor && (
+            <p className="services">
+              <Link to="https://google.com" style={{ color: "white" }}>
+                <ReactTyped
+                  strings={services}
+                  typeSpeed={40}
+                  backSpeed={50}
+                  startDelay={1000}
+                  loop
+                />
+              </Link>
+            </p>
+          )}
+        </div>
+      </section>
+    </Fragment>
   );
 };
 

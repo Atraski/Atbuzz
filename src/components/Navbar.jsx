64b8a49d9bd216aabf1f49673ec50689a759c41e
Draken-Dragon <bravotspiderman@gmail.com>
2024-07-11 12:46:55 +0530

    Change Navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,54 @@
-import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 import logo from "../assets/logo.png";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const [changeNavbar, setChangeNavbar] = useState(false);
+
+  const location = useLocation();
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setChangeNavbar(true);
+    } else {
+      setChangeNavbar(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const getStyleLink = (hash) => {
+    return location.hash === hash ? { color: "#ff288b" } : {};
+  };
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${changeNavbar ? "scrolledNav" : ""}`}>
       <div className="navbar-container container">
-        <NavLink to="/">
+        <a href="#home">
           <img src={logo} alt="Logo" />
-        </NavLink>
+        </a>
         <div className="nav-links">
-          <NavLink
-            to="/"
-            style={({ isActive }) => (isActive ? { color: "#a10202" } : {})}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/about"
-            style={({ isActive }) => (isActive ? { color: "#a10202" } : {})}
-          >
+          {/* <NavLink to="/">Home</NavLink> */}
+          <a href="#about" style={getStyleLink("#about")}>
             About Us
-          </NavLink>
-          <NavLink
-            to="/services"
-            style={({ isActive }) => (isActive ? { color: "#a10202" } : {})}
-          >
+          </a>
+          <a href="#services" style={getStyleLink("#services")}>
             Services
-          </NavLink>
-          <NavLink
-            to="/contact-us"
-            style={({ isActive }) => (isActive ? { color: "#a10202" } : {})}
-          >
+          </a>
+          <a href="#work" style={getStyleLink("#work")}>
+            Work
+          </a>
+          <a href="#contact" style={getStyleLink("#contact")}>
             Contact Us
-          </NavLink>
+          </a>
         </div>
       </div>
     </nav>

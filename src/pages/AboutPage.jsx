83d53266176f5942229ea1a1c30e8a27f8c42604
Draken-Dragon <bravotspiderman@gmail.com>
2024-07-11 12:44:04 +0530

    Update About us section.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,50 +1,61 @@
+import about from "../assets/about-us.jpg";
 import "../styles/AboutUsPage.css";
 
 const AboutPage = () => {
   return (
-    <section className="about-page container">
-      <h1 className="page-heading">About Us</h1>
-      <p>
-        Welcome to AtBuzz, your premier partner in navigating the dynamic world
-        of marketing. AtBuzz is a cutting-edge marketing agency committed to
-        transforming your brand’s presence and driving impactful results. Our
-        team of experts excels in crafting innovative strategies tailored to
-        meet your unique needs and propel your business to new heights.
-      </p>
-      <p>
-        In the digital age, staying ahead of the curve is essential. At AtBuzz,
-        we harness the power of the latest digital tools and techniques to
-        elevate your online presence. From SEO and SEM to social media marketing
-        and content creation, our comprehensive digital marketing services
-        ensure your brand captures the attention it deserves.
-      </p>
-      <p>
-        Building and maintaining a positive public image is crucial for any
-        business. Our seasoned PR professionals at AtBuzz specialize in creating
-        compelling narratives that resonate with your target audience. We help
-        you establish and nurture relationships with key stakeholders, ensuring
-        your brand's reputation remains stellar.
-      </p>
-      <p>
-        Leveraging the influence of key figures in your industry can amplify
-        your brand’s reach. At AtBuzz, we connect you with top influencers who
-        align with your brand values. Our strategic influencer marketing
-        campaigns are designed to engage and convert, driving authentic
-        interactions and lasting loyalty.
-      </p>
-      <p>
-        Bringing people together is at the heart of what we do. Whether it's a
-        corporate event, product launch, or an exhibition, AtBuzz creates
-        unforgettable experiences that leave a lasting impression. Our
-        meticulous planning and creative execution ensure your events are not
-        only successful but also memorable.
-      </p>
-      <p>
-        At AtBuzz, we are passionate about helping our clients succeed. Our
-        collaborative approach and dedication to excellence set us apart.
-        Partner with us and experience the buzz of transformative marketing
-        solutions. Together, we can achieve greatness.
-      </p>
+    <section className="about-page" id="about">
+      <div className="container">
+        <h1 className="page-heading">About Us</h1>
+        <div className="about-content-container">
+          <div className="image-container">
+            <img src={about} alt="" />
+          </div>
+          <div className="about-content">
+            <p>
+              Welcome to AtBuzz, your premier partner in navigating the dynamic
+              world of marketing. AtBuzz is a cutting-edge marketing agency
+              committed to transforming your brand’s presence and driving
+              impactful results. Our team of experts excels in crafting
+              innovative strategies tailored to meet your unique needs and
+              propel your business to new heights.
+            </p>
+            <p>
+              In the digital age, staying ahead of the curve is essential. At
+              AtBuzz, we harness the power of the latest digital tools and
+              techniques to elevate your online presence. From SEO and SEM to
+              social media marketing and content creation, our comprehensive
+              digital marketing services ensure your brand captures the
+              attention it deserves.
+            </p>
+            <p>
+              Building and maintaining a positive public image is crucial for
+              any business. Our seasoned PR professionals at AtBuzz specialize
+              in creating compelling narratives that resonate with your target
+              audience. We help you establish and nurture relationships with key
+              stakeholders, ensuring your brand's reputation remains stellar.
+            </p>
+            <p>
+              Leveraging the influence of key figures in your industry can
+              amplify your brand’s reach. At AtBuzz, we connect you with top
+              influencers who align with your brand values. Our strategic
+              influencer marketing campaigns are designed to engage and convert,
+              driving authentic interactions and lasting loyalty.
+            </p>
+            <p>
+              Bringing people together is at the heart of what we do. Whether
+              it's a corporate event, product launch, or an exhibition, AtBuzz
+              creates unforgettable experiences that leave a lasting impression.
+              Our meticulous planning and creative execution ensure your events
+              are not only successful but also memorable.
+            </p>
+            <p>
+              At AtBuzz, we are passionate about helping our clients succeed.
+              Our collaborative approach and dedication to excellence set us
+              apart.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };

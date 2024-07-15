13e28af7c2ccc303e7636794f970eae899155231
Draken-Dragon <bravotspiderman@gmail.com>
2024-07-15 11:38:49 +0530

    feat: Add responsive design with media queries for mobile view

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useMemo, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { ReactTyped } from "react-typed";
 
 import "../styles/HomePage.css";
